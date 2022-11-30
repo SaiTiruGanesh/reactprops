1442b15bde556e3ff35dd133b977dc70cb1f5459
chandini-mad <madme7635@gmail.com>
2022-11-30 16:52:15 +0530

    react commited

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+// import ClassComponent from './ClassComponent'
+import PropsExample from './PropsExample'
+ 
+function App(){
+  return(
+    <div>
+    <PropsExample name="ganesh" age="20"/>
+    <PropsExample name="sai" age="10"/>
+    {/*<p>Good Afternoon</p>
+    <h1>Bahubali </h1>
+    <ClassComponent/> 
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSLsan10V46TIs0_FpV4JsbNbngFU804GKn16uNH5D6Ulqx6-KiWghK7PwQG4WV-zhWGbg&usqp=CAU"width="50%"alt="bahubali"/>*/}
     </div>
-  );
+    )
 }
-
-export default App;
+export default App 
