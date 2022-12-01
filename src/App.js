2cc0bf82e7c32c294501cc1da7ad619b76c9cc9d
chandini-mad <madme7635@gmail.com>
2022-12-01 16:50:39 +0530

    states and events

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 // import ClassComponent from './ClassComponent'
-import PropsExample from './PropsExample'
- 
+//import PropsExample from './PropsExample'
+import StateExample from './StateExample'
+import FuncProps from './FuncProps'
+import Demo from './Demo'
+import Events from './Events'
+  
 function App(){
   return(
     <div>
-    <PropsExample name="ganesh" age="20"/>
+    <Events/>
+    {/*<StateExample/>
+
+    <Demo name="tiru"/>
+    <FuncProps carname="rolls royce" year="1998"/>
+    {<PropsExample name="ganesh" age="20"/>
     <PropsExample name="sai" age="10"/>
-    {/*<p>Good Afternoon</p>
+    {<p>Good Afternoon</p>
     <h1>Bahubali </h1>
     <ClassComponent/> 
-    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSLsan10V46TIs0_FpV4JsbNbngFU804GKn16uNH5D6Ulqx6-KiWghK7PwQG4WV-zhWGbg&usqp=CAU"width="50%"alt="bahubali"/>*/}
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSLsan10V46TIs0_FpV4JsbNbngFU804GKn16uNH5D6Ulqx6-KiWghK7PwQG4WV-zhWGbg&usqp=CAU"width="50%"alt="bahubali"/>}}*/}
     </div>
     )
 }
---
+++ src/Demo.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+class Demo extends React.Component{
+	state={
+			s:this.props.name
+		}
+	render(){
+		return(
+		<div>
+			<p>{this.state.s}</p>
+			<h1>{this.props.name}</h1>	
+		</div>
+		)
+	}
+		
+}
+export default Demo
